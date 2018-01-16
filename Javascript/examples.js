13a45843303c147a80e2621dd845aac6f78fced4
unknown <tpham20908@gmail.com>
2018-01-16 08:25:52 -0500

    reverseString

--- Javascript/examples.js
+++ Javascript/examples.js
@@ -1,48 +1,66 @@
 
-//Setup
-var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["Javascript", "Gaming", "Foxes"]
-    }
-];
+// //Setup
+// var contacts = [
+//     {
+//         "firstName": "Akira",
+//         "lastName": "Laine",
+//         "number": "0543236543",
+//         "likes": ["Pizza", "Coding", "Brownie Points"]
+//     },
+//     {
+//         "firstName": "Harry",
+//         "lastName": "Potter",
+//         "number": "0994372684",
+//         "likes": ["Hogwarts", "Magic", "Hagrid"]
+//     },
+//     {
+//         "firstName": "Sherlock",
+//         "lastName": "Holmes",
+//         "number": "0487345643",
+//         "likes": ["Intriguing Cases", "Violin"]
+//     },
+//     {
+//         "firstName": "Kristian",
+//         "lastName": "Vos",
+//         "number": "unknown",
+//         "likes": ["Javascript", "Gaming", "Foxes"]
+//     }
+// ];
 
 
-function lookUpProfile(firstName, prop){
-// Only change code below this line
-  for (var i = 0; i < contacts.length; i++) {
-    if (contacts[i].firstName === firstName) {
-      if (contacts[i].hasOwnProperty(prop)) {
-        return contacts[i][prop];
-      }
-      else {
-        return "No such property";
-      }
-    }
-  }
-  return "No such contact";
-// Only change code above this line
-}
+// function lookUpProfile(firstName, prop){
+// // Only change code below this line
+//   for (var i = 0; i < contacts.length; i++) {
+//     if (contacts[i].firstName === firstName) {
+//       if (contacts[i].hasOwnProperty(prop)) {
+//         return contacts[i][prop];
+//       }
+//       else {
+//         return "No such property";
+//       }
+//     }
+//   }
+//   return "No such contact";
+// // Only change code above this line
+// }
 
-// Change these values to test your function
-lookUpProfile("Sherlock", "likes");
+// // Change these values to test your function
+// lookUpProfile("Sherlock", "likes");
+
+var Bike = function() {
+
+  // Only change code below this line.
+  var gear;
+
+  this.setGear = function(gear) {
+    this.gear = gear;
+  };
+
+  this.getGear = function() {
+    return gear;
+  };
+};
+
+var myBike = new Bike();
+myBike.setGear(4);
+console.log(myBike.getGear());
